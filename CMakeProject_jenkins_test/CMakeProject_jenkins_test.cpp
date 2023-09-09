465cc5714722308d788571794a604eccac328abf
robosina <saeed.masoomi2013@gmail.com>
2023-09-09 13:46:55 +0330

    first commit

---
+++ CMakeProject_jenkins_test/CMakeProject_jenkins_test.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject_jenkins_test.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject_jenkins_test.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
